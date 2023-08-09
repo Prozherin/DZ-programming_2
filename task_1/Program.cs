6562bf76101a39aed4a0e18117d074574c1ae1b6
Prozherin <taskacik@gmail.com>
2023-08-09 18:00:30 +0500

    add task 1 (принимает трёхзначное число и выдает вторую цифру).

---
+++ task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// // 456 -> 5
+// // 782 -> 8
+// // 918 -> 1
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      // Также можно применить функцию Random
+      //   Random random = new Random();
+      //   int number = random.Next(100, 1000);
+      Console.WriteLine("Ведите трехзначное число: ");
+      int number = Convert.ToInt32(Console.ReadLine());
+
+        char[] digits = number.ToString().ToCharArray();
+
+      Console.WriteLine(digits[1]);
+    }
+}
